ecc775acf9e4e72e91314e0b1704aff30999af4a
MukundChourey <53388376+MukundChourey@users.noreply.github.com>
2024-04-06 15:41:20 +0530

    fix: vercel configs for api routes

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = ''; // it takes the current host
 
 async function register() {
     const name = document.getElementById("name").value.trim();
